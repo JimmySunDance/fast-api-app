cb3679b808524075fb65491995a2a1b189181456
James Poulten <jpoulten@me.com>
2022-01-13 10:34:40 +0000

    Add user tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,6 +3,7 @@ from jose import jwt
 from app import schemas
 from app.config import settings
 
+
 def test_root(client):
     res = client.get("/")
     assert res.json().get("message") == "Welcome to my API"
@@ -18,34 +19,33 @@ def test_crete_user(client):
     assert res.status_code == 201
 
 
-def test_login_user(client, test_user):
+def test_login_user(client, test_user_1):
     res = client.post(
-        "/login/", data={
-            "username": test_user["email"], 
-            "password": test_user["password"]
-        }
+        "/login/",
+        data={"username": test_user_1["email"], "password": test_user_1["password"]},
     )
     login_res = schemas.Token(**res.json())
-    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
+    )
     id = payload.get("user_id")
 
-    assert id == test_user["id"]
+    assert id == test_user_1["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
 
-@pytest.mark.parametrize("email, password, status_code", [
-    ("wrongEmail@test.com", "password123", 403),
-    ("testemail@test.com", "wrongPassword", 403),
-    ("wrongEmail@test.com", "wrongPassword", 403),
-    (None, "password123", 422),
-    ("testemail@test.com", None, 422), 
-])
-def test_failed_login(test_user, client, email, password, status_code):
-    res = client.post("/login/", 
-        data={
-            "username": email, 
-            "password": password
-        }
-    )
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongEmail@test.com", "password123", 403),
+        ("testemail@test.com", "wrongPassword", 403),
+        ("wrongEmail@test.com", "wrongPassword", 403),
+        (None, "password123", 422),
+        ("testemail@test.com", None, 422),
+    ],
+)
+def test_failed_login(client, email, password, status_code):
+    res = client.post("/login/", data={"username": email, "password": password})
 
     assert res.status_code == status_code
